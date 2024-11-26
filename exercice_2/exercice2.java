a34e4406fc004350762c19c2e805667ef565e593
Pierre-Mzuafo <mzuafofp@3il.fr>
2024-11-27 00:12:55 +0100

    exercice2

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -1,21 +1,35 @@
 public class GestionnaireNotes {
 
-    // Affiche note et moyennes
-    public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
-        // Affichage des notes
+     //calcul de la moyenne
+
+    public double moyenne(int [] notes)
+       {
+        double somme = 0;
+          for(int note : notes)
+          {
+            somme += note ;
+          }
+         double moy = somme / notes.length; 
+        return moy;
+       }   
+
+
+     // Affiche note et moyennes
+
+    public void afficherNotesEtMoyenne(String nomEtudiant, double[] notes) {
+
+     // Affichage des notes
+
         System.out.println("Notes de " + nomEtudiant + ":");
         for (int note : notes) {
             System.out.println("- " + note);
-        }
+         }
+    
+         double moyen;
 
-        // Calcul de la somme des notes
-        int somme = 0;
-        for (int note : notes) {
-            somme += note;
-        }
+         moyen = moyenne(notes); // recuperation de la moyenne
 
-        // Affichage de la moyenne
-        double moyenne = (double) somme / notes.length;
-        System.out.println("Moyenne : " + moyenne);
+         System.out.println("La moyenne de ses note est : " + moyen);
+ 
     }
 }
